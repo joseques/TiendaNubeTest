e8fe757b9ff04f93d7261328de582f9589bcbbc6
Jose <quesjose1@gmail.com>
2018-06-04 20:33:48 -0400

    Fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProductsModule } from './products/products.module';
 import { HomeComponent } from './home/home.component';
 const routes: Routes = [
-	{ path: '', component: HomeComponent },
 	{ path: 'products', loadChildren: './products/products.module#ProductsModule' },
-	{ path: 'products/new', loadChildren: './products/products.module#ProductsModule' }
+	{ path: 'products/new', loadChildren: './products/products.module#ProductsModule' },
+	{ path: '**', redirectTo: 'products' }
 ];
 
 @NgModule({
